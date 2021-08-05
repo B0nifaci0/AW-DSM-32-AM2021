31bbafc612b273aeeda532652687e8524f1b4fb6
Bonifacio <osx@192.168.1.12>
2021-08-05 08:46:18 -0500

    conexion a bd

---
+++ conexion.php
@@ -0,0 +1,30 @@
+<?php
+
+$server_name = 'localhost';
+$user = 'root';
+$password = 'root';
+$database = 'AWS32';
+$db_port = 8889;
+
+
+$mysqli = new mysqli(
+    $server_name,
+    $user,
+    $password,
+    $database
+  );
+  if ($mysqli->connect_error) {
+    echo 'Errno: '.$mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: '.$mysqli->connect_error;
+    exit();
+  }
+
+  echo 'Success: A proper connection to MySQL was made.';
+  echo '<br>';
+  echo 'Host information: '.$mysqli->host_info;
+  echo '<br>';
+  echo 'Protocol version: '.$mysqli->protocol_version;
+
+  $mysqli->close()
+  ?>
